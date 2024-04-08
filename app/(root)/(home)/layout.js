701b2cf03e9915422305734eabbfb64f3207ef0c
Faareh-Ahmed <faarehahmed@gmail.com>
2024-04-08 08:10:34 +0500

    Project Setup

---
+++ app/(root)/(home)/layout.js
@@ -0,0 +1,15 @@
+import Navbar from '@/components/navbar'
+import Sidebar from '@/components/sidebar'
+import React from 'react'
+
+const HomeLayout = () => {
+  return (
+    <div className=' bg-dark-1 text-white'>
+        <Navbar/>
+        <Sidebar/>
+        HomeLayout
+    </div>
+  )
+}
+
+export default HomeLayout
